558bb72ddb5357d3793390e17dd4f690b9a3b9c9
민형 <alsgud413@naver.com>
2022-11-02 22:44:45 +0900

    민형

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Main from './project/pages/Main';
 import Login from './project/Login/Login';
-import SignUp from './project/SingUp/SignUp';
 import Home from './project/HOME/Home';
+import MyPage from './project/pages/MyPage';
 import MemberInfo from './project/pages/MemberInfo';
+import MemberDrop from './project/pages/MemberDrop';
+import SignUp from './project/SingUp/SignUp';
+import Pg1 from './project/pages/pg1';
 
 
 function App() {
@@ -14,7 +17,10 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path='/signup' element={<SignUp />} />
         <Route path='/home' element={<Home />} />
+        <Route path='/mypage' element={<MyPage />} />
         <Route path='/MemberInfo' element={<MemberInfo />} />
+        <Route path='/MemberDrop' element={<MemberDrop />} />
+        <Route path='/Pg1' element={<Pg1 />} />
       </Routes>
     </Router>
   );
