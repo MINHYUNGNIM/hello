ce7ea1a7e9bad7862cbf2c8b39532652e46f50d5
민형 <alsgud413@naver.com>
2022-11-02 12:53:45 +0900

    민형

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Main from './project/pages/Main';
+import Login from './project/Login/Login';
+import SignUp from './project/SingUp/SignUp';
+import Home from './project/HOME/Home';
+import MemberInfo from './project/pages/MemberInfo';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/MemberInfo' element={<MemberInfo />} />
+      </Routes>
+    </Router>
   );
 }
 
